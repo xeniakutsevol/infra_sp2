cd915d2b4704cb8a5bfc8475117e32c71b023e96
Ксения Куцевол <xeniakutsevol@gmail.com>
2022-08-21 18:11:41 +0300

    changes after review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,14 +5,13 @@ from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default='False')
+# DEBUG = os.getenv('DEBUG', default='False')
+# Со строкой выше тесты не проходит, поэтому:
+DEBUG=False
 
 ALLOWED_HOSTS = ['*']
 
